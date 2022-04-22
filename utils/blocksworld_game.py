5d26c91e10368828cb4e6d019902530af2e21d62
Kevin Spevak <kevinspevak@gmail.com>
2022-04-22 17:44:37 -0600

    Add ASCII art display for game CLI

--- utils/blocksworld_game.py
+++ utils/blocksworld_game.py
@@ -2,6 +2,8 @@
 from .pddl import Domain, Problem
 from .planning_world import PlanningWorld
 from os.path import dirname, join
+import numpy as np
+import pdb
 
 class BlocksworldGame:
     def __init__(self):
@@ -16,7 +18,7 @@ class BlocksworldGame:
     def open_cli(self):
         print("Starting blocksworld game. 'quit' to quit")
         while True:
-            print("Current State:", self.world.state_expr())
+            self.display_state()
             command = input("enter action --> ")
             if command == "quit":
                 print("exiting game")
@@ -39,8 +41,32 @@ class BlocksworldGame:
             if self.world.is_legal(action, tuple(args)):
                 self.world.take_action(action, tuple(args))
                 if self.world.goal_satisfied():
+                    self.display_state()
                     print("Goal Satisfied!")
                     print("Final state", self.world.state_expr())
                     break # TODO start new problem?
             else:
                 print("Illegal action")
+
+
+    def display_state(self):
+        state = self.world.state_expr()
+        stacks = [(clause[1],) for clause in state if clause[0] == "ontable"]
+        holding = [clause[1] for clause in state if clause[0] == "holding"]
+        holding = "|"+holding[0]+"|" if len(holding) > 0 else ''
+        rest = [clause for clause in state if clause[0] == "on"]
+        while len(rest) > 0:
+            prev_level = [stack[-1] for stack in stacks]
+            next_clause = [clause for clause in rest if clause[2] in prev_level][0]
+            for i in range(len(stacks)):
+                if stacks[i][-1] == next_clause[2]:
+                    stacks[i] = stacks[i] + (next_clause[1],)
+            rest.remove(next_clause)
+        height = max([len(stack) for stack in stacks])
+        for y in range(height):
+            idx = height - y - 1
+            row = ["|"+stack[idx]+"|" if len(stack) > idx else "   "
+                   for stack in stacks]
+            endl = "" if idx == 0 else "\n"
+            print("".join(row), end=endl)
+        print("  (>"+holding+"<)===")
